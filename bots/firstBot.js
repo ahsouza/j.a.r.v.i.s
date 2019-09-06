6ecccd8d9ddff0530416ed7a042d6755409591ec
Aníbal Henrique <annibalhsouza@gmail.com>
2019-09-06 19:43:25 -0300

    #ISSUE_11 Adding examples of events Telegram

--- bots/firstBot.js
+++ bots/firstBot.js
@@ -5,10 +5,9 @@ const bot = new Telegraf(env.token)
 
 // Ao clicar no botão START de seu bot no aplicativo telegram, será executado a seguinte instrução:
 bot.start(context => {
-  const from = context.update.message.from
-  console.log(from)
-
-  context.reply(`Seja bem vindo, ${from.first_name}!`)
+  const name = context.update.message.from.first_name
+  
+  context.reply(`Seja bem vindo, ${name}!`)
 })
 
 // Ao digitar qualquer texto na janela de conversação do Bot terá a seguinte função assíncrona como resposta 
--- bots/toMasters.js
+++ bots/toMasters.js
@@ -13,5 +13,38 @@ bot.start(context => {
 
 })
 
+bot.on('text', context => {
+  context.reply(`Mensagem '${context.update.message.text}' recebido com sucesso!`)
+})
+
+bot.on('location', context => {
+  const location = 	context.update.message.location
+  console.log(location)
+  context.reply(`Entendido Senhor, as coordenadas de sua localização está em
+  	Lat: ${location.latitude},
+  	Lon: ${location.longitude}!`)
+})
+
+bot.on('contact', context => {
+  const contact = context.update.message.contact
+  console.log(contact)	
+  context.reply(`Lembrarei do(a)
+  	${contact.first_name} (${contact.phone_number})`)
+})
+
+bot.on('voice', content => {
+  const voice = context.update.message.voice
+  console.log(voice)
+  context.reply(`Audio recebido com sucesso, este aúdio possui ${voice.duration} segundos`)
+})
+
+bot.on('photo', content => {
+  const photo = context.update.message.sticker
+  console.log(sticker)	
+  content.reply(`Bacana! vejo que você enviou
+  	o ${sticker.emoji} do conjunto ${sticker.set_name}`)
+})
+
+
 // Start Bot
 bot.startPolling()
