37c1f40dd436aea5d9a6da2488548a88ab17f28c
Aníbal Henrique <annibalhsouza@gmail.com>
2019-09-06 21:44:44 -0300

    #ISSUE_15 Adding type replys for Location and Video

--- bots/typeReplys.js
+++ bots/typeReplys.js
@@ -6,7 +6,9 @@ bot.start(async content => {
   await content.reply(`Seja bem vindo, ${content.update.message.from.first_name}! 😃`)	
   await content.replyWithHTML(`Destacando mensagem <b>HTML</b> <i>de várias formas</i> <code>ahsouza</code> <a href='https://ahsouza.github.com'> Site</a>`)
   await content.replyWithMarkdown('Destacando mensagem com *Markdown*')
-  // await content.replyWithPhoto({source: `${__dirname}/assets/jarvis.jpg`})
+  await content.replyWithPhoto({source: `${__dirname}/assets/jarvis.jpg`})
+  await content.replyWithLocation(29.9773008, 31.1303068)
+  await content.replyWithVideo('https://www.youtube.com/watch?v=umokQ920a8U&list=LLm1wbADwaDcrQzpUz5uOZGQ&index=38')
 })
 
 bot.startPolling()
