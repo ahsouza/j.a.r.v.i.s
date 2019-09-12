675228c7fa3872a809a12da0eeb4c029ec613ec7
Aníbal Henrique <annibalhsouza@gmail.com>
2019-09-12 00:09:12 -0300

    #ISSUE_43 Adding keyboard to tools develop and replys

--- projects/studies/studies.js
+++ projects/studies/studies.js
@@ -13,11 +13,41 @@ const keyboardOptionsStudies = Markup.keyboard([
 
 ]).resize().extra()
 
+// *************************
+// *** KEYBOARD SOFTWARE ***
+// *************************
 const keyboardSoftwareDevelop = Markup.keyboard([
-  ['BACK-END', 'FRONT-END', 'DEV-OPS']
+  ['BACK-END', 'FRONT-END', 'DEV-OPS'],
+  ['MOBILE']
 
 ]).resize().extra()
 
+const keyboardBackEnd = Markup.keyboard([
+  ['NODE.JS', 'LARAVEL PHP', 'ASP.NET']
+
+]).resize().extra()
+
+const keyboardFrontEnd = Markup.keyboard([
+  ['VUE.JS', 'REACT.JS', 'ANGULAR.JS']
+
+]).resize().extra()
+
+const keyboardDevOps = Markup.keyboard([
+  ['ANSIBLE', 'DOCKER', 'JEKINS'],
+  ['KUBERNETES']
+
+]).resize().extra()
+
+const keyboardMobile = Markup.keyboard([
+  ['KOTLIN', 'REACT NATIVE']
+
+]).resize().extra()
+
+
+
+// *************************
+// *** KEYBOARD CLOUD ******
+// *************************
 const keyboardCloudServices = Markup.keyboard([
   ['AWS', 'AZURE', 'HEROKU'],
   ['GCP', 'OPENSHIFT4']
@@ -36,15 +66,39 @@ const location = Markup.keyboard([
 bot.start(async context=> {
   const name = context.update.message.from.first_name + ' ' + context.update.message.from.last_name	
   await context.replyWithMarkdown(`Olá Mestre *${name}*!`)
-  await context.replyWithMarkdown(`_Vamos estudar o que hoje?_`, keyboardOptionsStudies)
+  await context.replyWithMarkdown(`_Vamos estudar o que hoje Senhor?_`, keyboardOptionsStudies)
   // await context.replyWithPhoto('')
 })
 
+// *************************
+// **** REPLYS SOFTWARE ****
+// *************************
 bot.hears('</> Desenvolvimento De Software',async context => {
   context.replyWithMarkdown(`Ok Senhor! Qual área para Desenvolvimento De Software?`, keyboardSoftwareDevelop)
 })
 
-bot.hears('👾 Implantação De Serviços/Software', context => {
+
+bot.hears('BACK-END', async context => {
+  context.replyWithMarkdown(`Bacana Senhor! Vejamos o que temos a te oferecer no momento! 🙂`, keyboardBackEnd)	
+})
+
+bot.hears('FRONT-END', async context => {
+  context.replyWithMarkdown(`Legal Senhor! Querer algo mais leve é sempre bom, qual seria a tecnologia?`, keyboardFrontEnd)	
+})
+
+bot.hears('DEV-OPS', async context => {
+  context.replyWithMarkdown(`Ok Senhor! Qual ferramenta para integrar os Software?`, keyboardDevOps)	
+})
+
+bot.hears('MOBILE', async context => {
+  context.replyWithMarkdown(`Aplicativos? Ah Bacana! Senhor me diz a tecnologia desejada!`, keyboardMobile)	
+})
+
+
+// *************************
+// ******REPLYS CLOUD ******
+// *************************
+bot.hears('👾 Implantação De Serviços/Software',async context => {
   context.replyWithMarkdown(`Ok Senhor! Qual plataforma você deseja obter informações/tutoriais?`, keyboardCloudServices)
 })
 
