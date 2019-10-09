e1b32a320e2a2decf0cba73622d4be10c5daea98
Aníbal Henrique <annibalhsouza@gmail.com>
2019-10-09 20:06:37 -0300

    #ISSUE_80 Optimize code

--- projects/schedule/api/config/router.ts
+++ projects/schedule/api/config/router.ts
@@ -1,5 +1,17 @@
 import * as restify from 'restify'
 
 export abstract class Router {
-  abstract applyRoutes(application: restify.Server)	
+  abstract applyRoutes(application: restify.Server)
+
+  render(res: restify.Response, next: restify.Next) {
+    return (document) => {
+      if(document){
+        res.json(document)
+      } else {
+        res.send(404)
+      }
+
+      return next()
+    }
+  }
 }
--- projects/schedule/api/dist/config/router.js
+++ projects/schedule/api/dist/config/router.js
@@ -1,5 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 class Router {
+    render(res, next) {
+        return (document) => {
+            if (document) {
+                res.json(document);
+            }
+            else {
+                res.send(404);
+            }
+            return next();
+        };
+    }
 }
 exports.Router = Router;
