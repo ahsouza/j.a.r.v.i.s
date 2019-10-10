e5ca2a67fd1623d989927c08768dba7df939e40b
Aníbal Henrique <annibalhsouza@gmail.com>
2019-10-10 12:37:41 -0300

    #ISSUE_94 Extending routes and their user resources

---
+++ projects/schedule/api/dist/config/model-router.js
@@ -0,0 +1,58 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const router_1 = require("./router");
+const restify_errors_1 = require("restify-errors");
+class ModelRouter extends router_1.Router {
+    constructor(model) {
+        super();
+        this.model = model;
+        this.findAll = (req, res, next) => {
+            this.model.find()
+                .then(this.renderAll(res, next))
+                .catch(next);
+        };
+        this.findById = (req, res, next) => {
+            this.model.findById(req.params.id)
+                .then(this.render(res, next))
+                .catch(next);
+        };
+        this.save = (req, res, next) => {
+            let document = new this.model(req.body);
+            document.save()
+                .then(this.render(res, next))
+                .catch(next);
+        };
+        this.replace = (req, res, next) => {
+            const options = { runValidators: true, overwrite: true };
+            this.model.update({ _id: req.params.id }, req.body, options)
+                .exec().then(result => {
+                if (result.n) {
+                    return this.model.findById(req.params.id);
+                }
+                else {
+                    throw new restify_errors_1.NotFoundError('Documento não encontrado');
+                }
+            }).then(this.render(res, next))
+                .catch(next);
+        };
+        this.update = (req, res, next) => {
+            const options = { runValidators: true, new: true };
+            this.model.findByIdAndUpdate(req.params.id, req.body, options)
+                .then(this.render(res, next))
+                .catch(next);
+        };
+        this.del = (req, res, next) => {
+            this.model.remove({ _id: req.params.id }).exec().then((cmdResult) => {
+                if (cmdResult.result.n) {
+                    res.send(204);
+                    return next();
+                }
+                else {
+                    throw new restify_errors_1.NotFoundError('Documento não encontrado');
+                }
+                return next();
+            }).catch(next);
+        };
+    }
+}
+exports.ModelRouter = ModelRouter;
