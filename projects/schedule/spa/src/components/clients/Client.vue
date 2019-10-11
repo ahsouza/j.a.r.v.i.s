568d0a5a844b00575ccf6cbc5784ee1dcb8fd7ff
Aníbal Henrique <annibalhsouza@gmail.com>
2019-10-11 11:17:25 -0300

    #ISSUE_106_FRONT-END# Form to register clients completed

--- projects/schedule/spa/src/components/clients/Client.vue
+++ projects/schedule/spa/src/components/clients/Client.vue
@@ -1,5 +1,5 @@
 <template>
-
+<div>
   <v-carousel>
     <v-carousel-item v-for="(color, i) in colors" :key="color">
       <v-sheet :color="color" height="100%" tile>
@@ -7,7 +7,7 @@
         <v-row class="fill-height" align="center" justify="center">
           <div class="display-3">
             
-            <v-card class="mx-auto" max-width="375" tile>
+            <v-card class="mx-auto" max-width="330" tile>
               <v-img height="100%" src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg">
                 
                 <v-row align="end" class="fill-height">
@@ -40,7 +40,7 @@
               <v-btn class="mx-2" fab dark small color="green">
                 <v-icon dark>mdi-phone</v-icon>
               </v-btn>
-              
+
               <v-btn class="mx-2" fab dark small color="red">
                 <v-icon dark>mdi-gmail</v-icon>
               </v-btn>
@@ -52,58 +52,120 @@
 
     
     </v-carousel-item>
- 
   </v-carousel>
-  
-<!--   
-  
-  
-  
-  <v-stepper v-model="e1">
-    <v-stepper-header>
-      <v-stepper-step :complete="e1 > 1" step="1">Registra-se</v-stepper-step>
+  <br>
 
-      <v-divider></v-divider>
+<v-dialog v-model="dialog" persistent max-width="920px">
+    
+     <template align=center v-slot:activator="{ on }">
+           <div class="my-2" align=center>
+            <v-btn color="primary" fab v-on="on"><v-icon>mdi-plus</v-icon></v-btn>
+           </div>
+      </template>
+    
+      <v-card>
 
-      <v-stepper-step :complete="e1 > 2" step="2">Name of step 2</v-stepper-step>
 
-      <v-divider></v-divider>
+        <v-tabs v-model="tab" background-color="deep-purple accent-4" centered dark icons-and-text>
+            <v-tabs-slider></v-tabs-slider>
 
-      <v-stepper-step step="3">Name of step 3</v-stepper-step>
-    </v-stepper-header>
+            <v-tab href="#tab-1">
+              Pessoal
+              <v-icon>mdi-account-box</v-icon>
+            </v-tab>
 
-    <v-stepper-items>
-      <v-stepper-content step="1">
-        <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
+            <v-tab href="#tab-2">
+              Endereço
+              <v-icon>mdi-home</v-icon>
+            </v-tab>
 
-        <v-btn color="primary" @click="e1 = 2">
-          Continue
-        </v-btn>
+            <v-tab href="#tab-3">
+              PROJETOS
+              <v-icon>mdi-source-pull</v-icon>
+            </v-tab>
+          </v-tabs>
 
-        <v-btn text>Cancel</v-btn>
-      </v-stepper-content>
+          <v-tabs-items v-model="tab">
+            <v-tab-item v-for="i in 3" :key="i" :value="'tab-' + i">
+              <v-card flat>
+                <v-card-text>
 
-      <v-stepper-content step="2">
-        <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
+                <v-container>
+                  <!-- DADOS PESSOAL -->
+                  <v-row v-if="i == 1">
+                    <v-col cols="12" sm="6" md="6">
+                      <v-text-field v-model="firstName" label="Nome" required></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="6">
+                      <v-text-field v-model="lastName" label="Sobrenome" persistent-hint required></v-text-field>
+                    </v-col>
 
-        <v-btn color="primary" @click="e1 = 3">
-          Continue
-        </v-btn>
+                    <v-col cols="6">
+                      <v-text-field label="Email" required></v-text-field>
+                    </v-col>
+                    <v-col cols="6">
+                      <v-text-field label="CPF" required></v-text-field>
+                    </v-col>
 
-        <v-btn text>Cancel</v-btn>
-      </v-stepper-content>
+                    <v-col cols="6">
+                      <v-text-field label="Telefone" required></v-text-field>
+                    </v-col>
+                    <v-col cols="6">
+                      <v-text-field label="Site" required></v-text-field>
+                    </v-col>
 
-      <v-stepper-content step="3">
-        <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
+                    <v-col cols="12" sm="6">
+                      <v-select :items="items" label="Redes Sociais" required></v-select>
+                    </v-col>
+                    <v-col cols="12" sm="6">
+                      <v-text-field :label="social" hint="conta do facebook" placeholder="https://facebook.com/" required></v-text-field>
+                    </v-col>
+                  </v-row>
+                  <!-- // LOCALIZAÇÃO -->
+                  <v-row v-if="i == 2">
+                    <v-col cols="12" sm="6">
+                      <v-select :paises="paises" label="País" required></v-select>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="6">
+                      <v-text-field label="Cidade" hint="cidade que o cliente mora atualmente" persistent-hint required></v-text-field>
+                    </v-col>
+                    
+                    <v-col cols="12" sm="6">
+                      <v-select :estado="estado" label="Estado" required></v-select>
+                    </v-col>
+                    <v-col cols="12" sm="6">
+                      <v-text-field v-model="postalCode" label="CEP" required></v-text-field>
+                    </v-col>
+                    <v-col cols="12">
+                      <v-text-field v-model="streetAddress" label="Logradouro" required></v-text-field>
+                    </v-col>
+                  </v-row>
+                  <!-- // PROJETOS -->
+                  <v-row v-if="i == 3">
+                    <v-col cols="12" sm="6" md="6">
+                      <v-select label="Projetos" required></v-select>
+                    </v-col>
+            
+                    <v-col cols="12" sm=6 md=6>
+                      <v-text-field label="URL Git"></v-text-field>
+                    </v-col>
+                  </v-row>
 
-        <v-btn color="primary" @click="e1 = 1">
-          Continue
-        </v-btn>
+                </v-container>
+                </v-card-text>
+                <v-card-actions>
+                  <div class="flex-grow-1"></div>
+                 <v-btn color="red darken-4" text @click="dialog = false">cancelar</v-btn>
+                 <v-btn color="success" text @click="dialog = false">salvar</v-btn>
+                </v-card-actions>
 
-        <v-btn text>Cancelar</v-btn>
-      </v-stepper-content>
-    </v-stepper-items>
-  </v-stepper> -->
+              </v-card>
+            </v-tab-item>
+          </v-tabs-items>
+
+      </v-card>
+    </v-dialog>
+</div>
 </template>
 
 
@@ -116,13 +178,11 @@
     data () {
       return {
         e1: 0,
-        colors: [
-          'primary',
-          'secondary',
-          'yellow darken-2',
-          'red',
-          'orange',
-        ],
+        tab: null,
+        dialog: false,
+        items: ['Facebook', 'LinkedIn', 'Instagram', 'Telegram', 'Twitter'],
+        colors: [ 'secondary', 'deep-purple accent-4', 'primary', 'yellow darken-2', 'orange'],
+        paises: ['Argentina', 'Alemanhã', 'Austrália', 'Brasil', 'Bolívia', 'Bulgária', 'Bangladesh', 'Canadá', 'Costa Rica'],
       }
     },
   }
